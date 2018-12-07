a945e7b52ed32bf7255829951920818c6d9b0a15
Andrii Vasyliev <sol@hiqdev.com>
2018-12-07 15:46:29 +0000

    Fixed code style in config

--- config/console.php
+++ config/console.php
@@ -2,26 +2,40 @@
 
 return [
     'app' => [
-        '__class' => yii\console\Application::class,
-        'controllerNamespace' => yii\console\controllers::class,
+        '__class' => \yii\console\Application::class,
+        'controllerNamespace' => \yii\console\controllers::class,
         'controllerMap' => [
-            'asset'     => yii\console\controllers\AssetController::class,
-            'cache'     => yii\console\controllers\CacheController::class,
-            'fixture'   => yii\console\controllers\FixtureController::class,
-            'help'      => yii\console\controllers\HelpController::class,
-            'message'   => yii\console\controllers\MessageController::class,
-            'migrate'   => yii\console\controllers\MigrateController::class,
-            'serve'     => yii\console\controllers\ServeController::class,
+            'asset' => [
+                '__class' => \yii\console\controllers\AssetController::class,
+            ],
+            'cache' => [
+                '__class' => \yii\console\controllers\CacheController::class,
+            ],
+            'fixture' => [
+                '__class' => \yii\console\controllers\FixtureController::class,
+            ],
+            'help' => [
+                '__class' => \yii\console\controllers\HelpController::class,
+            ],
+            'message' => [
+                '__class' => \yii\console\controllers\MessageController::class,
+            ],
+            'migrate' => [
+                '__class' => \yii\console\controllers\MigrateController::class,
+            ],
+            'serve' => [
+                '__class' => \yii\console\controllers\ServeController::class,
+            ],
         ],
     ],
 
     'request' => [
-        '__class' => yii\console\Request::class,
+        '__class' => \yii\console\Request::class,
     ],
     'response' => [
-        '__class' => yii\console\Response::class,
+        '__class' => \yii\console\Response::class,
     ],
     'errorHandler' => [
-        '__class' => yii\console\ErrorHandler::class,
+        '__class' => \yii\console\ErrorHandler::class,
     ],
 ];
