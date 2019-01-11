e6252c2cd4e3c98c510404edc763c26dcf31e311
Andrii Vasyliev <sol@hiqdev.com>
2019-01-11 18:06:30 +0000

    Quickfixed config merging problem with `UnsetArrayValue` crutch
    Still looking for better solution.

---
+++ config/tests.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'response' => [
+        'formatters' => new \yii\helpers\UnsetArrayValue(),
+    ],
+    'request' => [
+        'cookieValidationKey' => new \yii\helpers\UnsetArrayValue(),
+    ],
+];
