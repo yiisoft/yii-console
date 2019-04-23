03a31377adce9fb59cde258101c164cfd9f39664
Mehdi Achour <machour@gmail.com>
2019-04-23 19:03:01 +0100

    Put back correct configuration
    Build will work once https://github.com/hiqdev/composer-config-plugin/pull/28 is merged

--- config/tests.php
+++ config/tests.php
@@ -1,4 +1,10 @@
 <?php
 
 return [
+    'response' => [
+        'formatters' => new \Yiisoft\Arrays\UnsetArrayValue(),
+    ],
+    'request' => [
+       'cookieValidationKey' => new \Yiisoft\Arrays\UnsetArrayValue(),
+    ],
 ];
