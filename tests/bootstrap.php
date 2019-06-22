d68daf8780539d1365fe1b3bc6250f9cd4a0ecb1
Alexander Makarov <sam@rmcreative.ru>
2019-06-22 12:22:22 +0300

    Cleanup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,10 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
-
 use hiqdev\composer\config\Builder;
 use yii\di\Container;
 use yii\helpers\Yii;
 
 // ensure we get report on all possible php errors
 error_reporting(E_ALL);
-
-define('YII_ENABLE_ERROR_HANDLER', false);
-define('YII_DEBUG', true);
-define('YII_ENV', 'test');
 
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
