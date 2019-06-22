9a35b6ebd9df4a98742959ee725df85582ae2ad0
Alexander Makarov <sam@rmcreative.ru>
2019-06-22 11:59:07 +0300

    Move adding command to container

--- config/console.php
+++ config/console.php
@@ -1,7 +1,15 @@
 <?php
 
 return [
-    'app' => function () {
-        return new \Yiisoft\Yii\Console\Application();
+    'app' => function (\Psr\Container\ContainerInterface $container) {
+        $app = new \Yiisoft\Yii\Console\Application();
+        $loader = new \Symfony\Component\Console\CommandLoader\ContainerCommandLoader($container, [
+            'serve' => 'serve',
+        ]);
+        $app->setCommandLoader($loader);
+        return $app;
     },
+
+    // commands
+    'serve' => \Yiisoft\Yii\Console\Command\Serve::class,
 ];
