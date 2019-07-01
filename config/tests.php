f5a6d841ac837ab0418d5bccb9c917dd997690b5
Alexander Makarov <sam@rmcreative.ru>
2019-07-01 15:22:33 +0300

    Remove web config

--- config/tests.php
+++ config/tests.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-    'response' => [
-        'formatters' => new \Yiisoft\Arrays\UnsetArrayValue(),
-    ],
-    'request' => [
-       'cookieValidationKey' => new \Yiisoft\Arrays\UnsetArrayValue(),
-    ],
     'cache' => function () {
         return new Yiisoft\Cache\Cache(new \Yiisoft\Cache\ArrayCache());
     }
