cf5a30e08adc85b3f04d8e707ce8e56b2db80a2c
Bulat Damdinov <dbulats88@gmail.com>
2019-07-12 02:28:03 +0800

    #30 merge-able config

--- config/console.php
+++ config/console.php
@@ -3,13 +3,14 @@
 return [
     'app' => function (\Psr\Container\ContainerInterface $container) {
         $app = new \Yiisoft\Yii\Console\Application();
-        $loader = new \Symfony\Component\Console\CommandLoader\ContainerCommandLoader($container, [
-            'serve' => 'serve',
-        ]);
+        $loader = new \Symfony\Component\Console\CommandLoader\ContainerCommandLoader(
+            $container,
+            $container->get('commands')
+        );
         $app->setCommandLoader($loader);
         return $app;
     },
-
-    // commands
-    'serve' => \Yiisoft\Yii\Console\Command\Serve::class,
+    'commands' => [
+        'serve' => \Yiisoft\Yii\Console\Command\Serve::class,
+    ],
 ];
