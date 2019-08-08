e43a0a337c9c9270d663506aeb7df459b35070db
Alexander Makarov <sam@rmcreative.ru>
2019-08-08 15:03:42 +0300

    Fix config

--- config/console.php
+++ config/console.php
@@ -4,6 +4,8 @@ use Yiisoft\Yii\Console\Application;
 return [
     Application::class => function (\Psr\Container\ContainerInterface $container) use ($params) {
         $app = new \Yiisoft\Yii\Console\Application();
+        $commands = $container->get('commands');
+
         $loader = new \Symfony\Component\Console\CommandLoader\ContainerCommandLoader(
             $container,
             $params['commands']
@@ -14,4 +16,7 @@ return [
     'commands' => new \Yiisoft\Factory\Definitions\ValueDefinition([
         'serve' => \Yiisoft\Yii\Console\Command\Serve::class,
     ]),
+
+    // TODO: there should be no need for it
+    \Yiisoft\Yii\Console\Command\Serve::class => \Yiisoft\Yii\Console\Command\Serve::class
 ];
