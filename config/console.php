210da2f1623d46fb92ef1f6efe6fa7159c6754d1
Alexander Makarov <sam@rmcreative.ru>
2019-09-09 02:05:30 +0300

    Fix serve command

--- config/console.php
+++ config/console.php
@@ -1,14 +1,19 @@
 <?php
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\CommandLoader\ContainerCommandLoader;
 use Yiisoft\Yii\Console\Application;
+use Yiisoft\Yii\Console\Command\Serve;
 
 return [
-    Application::class => function (\Psr\Container\ContainerInterface $container) use ($params) {
-        $app = new \Yiisoft\Yii\Console\Application();
-        $loader = new \Symfony\Component\Console\CommandLoader\ContainerCommandLoader(
+    Application::class => function (ContainerInterface $container) use ($params) {
+        $app = new Application();
+        $loader = new ContainerCommandLoader(
             $container,
             $params['commands']
         );
         $app->setCommandLoader($loader);
         return $app;
     },
+    Serve::class => Serve::class,
 ];
