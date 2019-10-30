d460545bae512a6735095fdc02ae7c5e44dc4c31
roxblnfk <roxblnfk@mail.ru>
2019-10-30 20:37:09 +0300

    Add friendly exception support (#41)

--- src/Application.php
+++ src/Application.php
@@ -1,6 +1,10 @@
 <?php
 namespace Yiisoft\Yii\Console;
 
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\StyleInterface;
+use Yiisoft\FriendlyException\FriendlyExceptionInterface;
+
 class Application extends \Symfony\Component\Console\Application
 {
     public const VERSION = '1.0.0';
@@ -8,5 +12,24 @@ class Application extends \Symfony\Component\Console\Application
     public function __construct(string $name = 'Yii Console', string $version = self::VERSION)
     {
         parent::__construct($name, $version);
+    }
+
+    public function doRenderException(\Exception $e, OutputInterface $output)
+    {
+        parent::doRenderException($e, $output);
+        // Friendly Exception support
+        if ($e instanceof FriendlyExceptionInterface) {
+            if ($output instanceof StyleInterface) {
+                $output->title($e->getName());
+                $output->note($e->getSolution());
+                $output->newLine();
+            } else {
+                $output->writeln([
+                    '<fg=red>' . $e->getName() . '</>',
+                    '<fg=yellow>' . $e->getSolution() . '</>',
+                    '',
+                ]);
+            }
+        }
     }
 }
