ce6d737a42b5f0b6211c4629c52c6ccf48f355d5
viktorprogger <viktorprogger@gmail.com>
2020-01-24 13:37:23 +0300

    Fix StyleCI issue [skip-ci]

--- config/params.php
+++ config/params.php
@@ -6,7 +6,7 @@ return [
     'commands' => [
         'serve' => \Yiisoft\Yii\Console\Command\Serve::class,
     ],
-    'container' => static function():Yiisoft\Di\Container {
+    'container' => static function (): Yiisoft\Di\Container {
         if (!class_exists('Yiisoft\Di\Container')) {
             $message = 'You must either install yiisoft/di or create "container" configuration parameter'
                 . ' containing a callable which will return configured implementation of PSR-11 DI container.';
