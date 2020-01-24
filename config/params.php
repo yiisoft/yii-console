3bae74188cff5854d1c7dfaa7aa0dcb93131e120
viktorprogger <viktorprogger@gmail.com>
2020-01-24 13:27:16 +0300

    An option to fix DI container dependency

--- config/params.php
+++ config/params.php
@@ -6,4 +6,13 @@ return [
     'commands' => [
         'serve' => \Yiisoft\Yii\Console\Command\Serve::class,
     ],
+    'container' => static function():Yiisoft\Di\Container {
+        if (!class_exists('Yiisoft\Di\Container')) {
+            $message = 'You must either install yiisoft/di or create "container" configuration parameter'
+                . ' containing a callable which will return configured implementation of PSR-11 DI container.';
+            throw new \hiqdev\composer\config\exceptions\BadConfigurationException($message);
+        }
+
+        return new Yiisoft\Di\Container(require \hiqdev\composer\config\Builder::path('console'));
+    }
 ];
