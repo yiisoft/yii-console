cd22bd20e3d6b24a6e91bdcde947be93c44da452
Alexander Makarov <sam@rmcreative.ru>
2020-01-26 01:53:42 +0300

    Adjust config structure, fix dependencies, cleanup

--- config/params.php
+++ config/params.php
@@ -1,18 +1,24 @@
 <?php
 
-return [
-    'app.id' => 'yii-console',
-    'app.name' => 'Yii Console',
-    'commands' => [
-        'serve' => \Yiisoft\Yii\Console\Command\Serve::class,
-    ],
-    'container' => static function (): Yiisoft\Di\Container {
-        if (!class_exists('Yiisoft\Di\Container')) {
-            $message = 'You must either install yiisoft/di or create "container" configuration parameter'
-                . ' containing a callable which will return configured implementation of PSR-11 DI container.';
-            throw new \hiqdev\composer\config\exceptions\BadConfigurationException($message);
-        }
+use hiqdev\composer\config\Builder;
+use Psr\Container\ContainerInterface;
+use Yiisoft\Yii\Console\Command\Serve;
 
-        return new Yiisoft\Di\Container(require \hiqdev\composer\config\Builder::path('console'));
-    }
+return [
+    'console' => [
+        'id' => 'yii-console',
+        'name' => 'Yii Console',
+        'commands' => [
+            'serve' => Serve::class,
+        ],
+        'container' => static function (): ContainerInterface {
+            if (!class_exists(Yiisoft\Di\Container::class)) {
+                $message = 'You must either install yiisoft/di or create "container" configuration parameter'
+                    . ' containing a callable which will return configured implementation of PSR-11 DI container.';
+                throw new \RuntimeException($message);
+            }
+
+            return new Yiisoft\Di\Container(require Builder::path('console'));
+        }
+    ],
 ];
