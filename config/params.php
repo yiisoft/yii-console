ac1893c89cd2a788486d1b8f19f60359a2dae475
Alexander Makarov <samdark@users.noreply.github.com>
2020-09-11 17:58:54 +0000

    Apply fixes from StyleCI

--- config/params.php
+++ config/params.php
@@ -12,6 +12,6 @@ return [
             'serve' => Serve::class,
         ],
         'version' => '3.0',
-        'rebuildConfig' => static fn() => getenv('APP_ENV') === 'dev',
+        'rebuildConfig' => static fn () => getenv('APP_ENV') === 'dev',
     ],
 ];
