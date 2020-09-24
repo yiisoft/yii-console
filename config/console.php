ab3ed168bbcac8f43e6d6813d298bb207b348339
Andrii Vasyliev <sol@hiqdev.com>
2020-09-24 08:44:28 +0000

    Remove now unneeded config for ContainerInterface

--- config/console.php
+++ config/console.php
@@ -2,10 +2,5 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
-
 return [
-    ContainerInterface::class => static function (ContainerInterface $container) {
-        return $container;
-    },
 ];
