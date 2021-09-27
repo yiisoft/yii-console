4a2108b5b17de8f24a7fadedf6c2a775097867aa
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-09-27 14:51:49 +0300

    Fix #125: Support command aliases (#127)

--- src/CommandLoader.php
+++ src/CommandLoader.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Console;
 
 use Psr\Container\ContainerInterface;
+use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Command\LazyCommand;
 use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
@@ -15,17 +16,29 @@ final class CommandLoader implements CommandLoaderInterface
     private ContainerInterface $container;
 
     /**
-     * @var array<string, string>
+     * @psalm-var array<string, array{
+     *         name: non-empty-string,
+     *         aliases: non-empty-string[],
+     *         class: class-string<Command>,
+     *         hidden: bool,
+     *     }>
      */
     private array $commandMap;
 
     /**
-     * @param array<string, string> $commandMap An array with command names as keys and service ids as values
+     * @var string[]
+     */
+    private array $commandNames;
+
+    /**
+     * @param array $commandMap An array with command names as keys and service ids as values
+     *
+     * @psalm-param array<string, class-string> $commandMap
      */
     public function __construct(ContainerInterface $container, array $commandMap)
     {
         $this->container = $container;
-        $this->commandMap = $commandMap;
+        $this->setCommandMap($commandMap);
     }
 
     public function get(string $name)
@@ -34,56 +47,104 @@ final class CommandLoader implements CommandLoaderInterface
             throw new CommandNotFoundException(sprintf('Command "%s" does not exist.', $name));
         }
 
-        /** @var Command $commandClass */
-        $commandClass = $this->commandMap[$name];
+        $commandName = $this->commandMap[$name]['name'];
+        $commandAliases = $this->commandMap[$name]['aliases'];
+        $commandClass = $this->commandMap[$name]['class'];
+        $commandHidden = $this->commandMap[$name]['hidden'];
+
         $description = $commandClass::getDefaultDescription();
 
         if ($description === null) {
             return $this->getCommandInstance($name);
         }
 
-        $aliases = [];
-        $hidden = false;
-        $commandName = $commandClass::getDefaultName();
-
-        if ($commandName !== null) {
-            $aliases = explode('|', $commandName);
-            $primaryName = array_shift($aliases);
-
-            if ($primaryName === '') {
-                $hidden = true;
-            }
-        }
-
         return new LazyCommand(
-            $name,
-            $aliases,
+            $commandName,
+            $commandAliases,
             $description,
-            $hidden,
+            $commandHidden,
             function () use ($name) {
                 return $this->getCommandInstance($name);
             }
         );
     }
 
+    public function has(string $name): bool
+    {
+        return isset($this->commandMap[$name]) && $this->container->has($this->commandMap[$name]['class']);
+    }
+
+    public function getNames(): array
+    {
+        return $this->commandNames;
+    }
+
     private function getCommandInstance(string $name): Command
     {
+        $commandName = $this->commandMap[$name]['name'];
+        $commandClass = $this->commandMap[$name]['class'];
+        $commandAliases = $this->commandMap[$name]['aliases'];
+
         /** @var Command $command */
-        $command = $this->container->get($this->commandMap[$name]);
-        if ($command->getName() !== $name) {
-            $command->setName($name);
+        $command = $this->container->get($commandClass);
+
+        if ($command->getName() !== $commandName) {
+            $command->setName($commandName);
+        }
+        if ($command->getAliases() !== $commandAliases) {
+            $command->setAliases($commandAliases);
         }
 
         return $command;
     }
 
-    public function has(string $name)
+    /**
+     * @psalm-param array<string, class-string> $commandMap
+     */
+    private function setCommandMap(array $commandMap): void
     {
-        return isset($this->commandMap[$name]) && $this->container->has($this->commandMap[$name]);
+        $this->commandMap = [];
+        $this->commandNames = [];
+
+        foreach ($commandMap as $name => $class) {
+            $aliases = explode('|', $name);
+
+            $hidden = false;
+            if ($aliases[0] === '') {
+                $hidden = true;
+                array_shift($aliases);
+            }
+
+            $this->validateAliases($aliases);
+
+            $primaryName = array_shift($aliases);
+
+            $item = [
+                'name' => $primaryName,
+                'aliases' => $aliases,
+                'class' => $class,
+                'hidden' => $hidden,
+            ];
+
+            $this->commandMap[$primaryName] = $item;
+            $this->commandNames[] = $primaryName;
+            foreach ($aliases as $alias) {
+                $this->commandMap[$alias] = $item;
+            }
+        }
     }
 
-    public function getNames()
+    /**
+     * @psalm-param string[] $aliases
+     *
+     * @psalm-assert non-empty-string[] $aliases
+     */
+    private function validateAliases(array $aliases): void
     {
-        return array_keys($this->commandMap);
+        foreach ($aliases as $alias) {
+            if ($alias === '') {
+                throw new RuntimeException('Do not allow empty command name or alias.');
+            }
+        }
     }
 }
