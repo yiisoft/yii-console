c210bb9277bd49b1181e5ad55e8e853691084714
Alexander Makarov <sam@rmcreative.ru>
2023-05-28 18:30:20 +0300

    Fix list command (#183)

---
+++ src/Command/Game.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * @codeCoverageIgnore
+ */
+final class Game extends Command
+{
+    protected static $defaultName = '|yii';
+    protected static $defaultDescription = 'A Guessing Game';
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $steps = 1;
+        $number = random_int(0, 100);
+
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Welcome to the Guessing Game!');
+
+
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+        $question = new Question('Please enter a number between 0 and 100: ');
+
+        while (true) {
+            $answer = (int) $helper->ask($input, $output, $question);
+
+            if ($answer === $number) {
+                $io->success('You win! You guessed the number in ' . $steps . ' steps.');
+                return 0;
+            }
+
+            $steps++;
+            if ($answer > $number) {
+                $io->warning('Too high!');
+            } else {
+                $io->warning('Too low!');
+            }
+        }
+    }
+}
