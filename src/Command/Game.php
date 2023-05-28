56092a8b30867d6b7607daf9f1c6c66f352d170d
Alexander Makarov <sam@rmcreative.ru>
2023-05-28 19:24:20 +0400

    Fix psalm

--- src/Command/Game.php
+++ src/Command/Game.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
@@ -24,6 +25,7 @@ final class Game extends Command
         $io->title('Welcome to the Guessing Game!');
 
 
+        /** @var QuestionHelper $helper */
         $helper = $this->getHelper('question');
         $question = new Question('Please enter a number between 0 and 100: ');
 
