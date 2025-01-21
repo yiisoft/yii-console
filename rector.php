0243f220b33562dce18ae89d0443f89d818e70fd
Alexander Schranz <alexander@sulu.io>
2025-01-21 16:44:17 +0100

    Remove unneeded classes

--- rector.php
+++ rector.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
     ->withPaths([
