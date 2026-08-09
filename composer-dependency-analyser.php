7ff04e476faa17e24095ccddfee5956724e18067
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-09 20:09:46 +0300

    Add Composer Dependency Analyser (#241)

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/config', isDev: false)
+    ->addPathToScan(__DIR__ . '/src', isDev: false)
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    // `yiisoft/definitions` is used only in `config/di-console.php`, which are loaded by
+    // consumers using `yiisoft/di`, that already requires `yiisoft/definitions` itself.
+    ->ignoreErrorsOnPackageAndPath('yiisoft/definitions', __DIR__ . '/config/di-console.php', [ErrorType::SHADOW_DEPENDENCY])
+    // `ext-sockets` is optional in test
+    ->ignoreErrorsOnExtensionAndPath('ext-sockets', __DIR__ . '/tests/Command/Serve/ServeTest.php', [ErrorType::SHADOW_DEPENDENCY]);
